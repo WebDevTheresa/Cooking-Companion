75d7fa385c3cbbff2e8606769213ede9118021c2
“Theresa <webdevtheresa@gmail.com>
2023-08-08 20:41:39 -0400

    foodSelection component created

---
+++ client/src/FoodSelection.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import styled from 'styled-components';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const FoodSelection = () => {};
+
+export default FoodSelection;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,23 +1,75 @@
 import styled from 'styled-components';
+import cooking from './pictures/cooking.png';
+import { useNavigate } from 'react-router-dom';
 // import react from 'react';
 
+//fix responsiveness for background image
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/FoodSelection');
+  };
   return (
     <MainWrapper>
       <ElementsWrapper>
-        <h1>Cooking Companion</h1>
-        <button>Enter</button>
+        <Header>
+          Cooking <br></br>Companion
+        </Header>
+        {/* <h2>
+          Discover the Chef in You: Create Delicious Meals from Your
+          Ingredients, Effortlessly.
+        </h2> */}
+        <Button onClick={handleButtonClick}>Enter</Button>
+        <p>Cooking Simplified: Your Ingredients, Endless Possibilities.</p>
       </ElementsWrapper>
     </MainWrapper>
   );
 };
 
 const ElementsWrapper = styled.div`
+  background-image: url(${cooking});
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+
+  @media screen and (max-width: 768px) {
+    body {
+      background-size: inherit;
+    }
+  }
+
+  /* width: 100vh; */
+  /* background-color: orange; */
+`;
+
+const MainWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
 `;
 
-const MainWrapper = styled.div``;
+const Button = styled.button`
+  height: 77px;
+  font-size: 43px;
+  width: 221px;
+  background-color: #524333;
+  color: #e3d7ba;
+  border: none;
+  border-radius: 38px;
+  color: white;
+  margin-left: 80px;
+
+  &:hover {
+    background-color: #635b48;
+  }
+`;
+
+const Header = styled.h1`
+  font-size: 87px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #524333;
+  margin-left: 80px;
+`;
 
 export default Home;
