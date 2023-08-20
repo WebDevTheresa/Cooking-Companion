063a7329e9e1a6718313c3d4ea41fb2c0fcac9a7
“Theresa <webdevtheresa@gmail.com>
2023-08-20 19:39:41 -0400

    load recipes- useeffect added, not complete

--- client/src/LoadRecipe.js
+++ client/src/LoadRecipe.js
@@ -2,38 +2,36 @@ import styled from 'styled-components';
 import react from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import hands from './pictures/hands.png';
+import cooking from './pictures/cooking.png';
 import AccordionItem from './AccordionItem';
+import Feed from './Feed';
 
 const LoadRecipe = () => {
   const navigate = useNavigate();
-
+  const [ShowRecipes, setShowRecipe] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  console.log(ingredients, 'ingredients');
   const returnHome = () => {
     navigate(`/FoodSelection`);
   };
 
+  useEffect(() => {
+    const ingredientsInStorage = localStorage.getItem('ingredients');
+    // console.log(ingredientsInStorage, 'ingredients in storage');
+  }, []);
+
   return (
     <MainWrapper>
-      <Backbutton onClick={returnHome}>⮐</Backbutton>
+      <Feed />
     </MainWrapper>
   );
 };
-const Backbutton = styled.button`
-  background-color: #666600;
-  margin: 10px;
-  height: 50px;
-  text-align: center;
-  background-size: 200% auto;
-  color: white;
-  border-radius: 10px;
-  display: block;
-`;
 
 const MainWrapper = styled.div`
   margin: 0;
-  background-image: url(${hands});
-  background-repeat: repeat;
-  background-size: repeat;
+  background-image: url(${cooking});
+  /* background-repeat: repeat; */
+  background-size: cover;
   height: 100vh;
   position: relative;
   @media screen and (max-width: 768px) {
