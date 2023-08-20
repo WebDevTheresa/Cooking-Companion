dba181a6e3346c1364df9af8e0db259b737dd9e4
“Theresa <webdevtheresa@gmail.com>
2023-08-20 19:40:25 -0400

    component created

---
+++ client/src/SideBar.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import react from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import PostRecipe from './PostRecipe';
+const Sidebar = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/PostRecipe`);
+  };
+
+  return (
+    <div>
+      <Wrapper>
+        <NavigationLink to="/"> Home</NavigationLink>
+        <NavigationLink to="/FoodSelection"> Ingredients</NavigationLink>
+
+        <AddRecipeButton onClick={handleClick}>Add My Recipe</AddRecipeButton>
+      </Wrapper>
+    </div>
+  );
+};
+
+const NavigationLink = styled(NavLink)`
+  font-size: 24px;
+  font-weight: bold;
+  margin: 10px;
+  text-decoration: none;
+  color: black;
+
+  &:hover {
+    color: #c47d1f;
+    width: 70%;
+    border-radius: 12px;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 80px;
+  background-color: #f7f0e6;
+  padding: 30px;
+  border-radius: 30px;
+`;
+
+const AddRecipeButton = styled.button`
+  background-image: linear-gradient(
+    to right,
+    #649173 0%,
+    #dbd5a4 51%,
+    #649173 100%
+  );
+  margin: 10px;
+  padding: 15px 45px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  display: block;
+
+  &:hover {
+    background-position: right center;
+    color: #fff;
+    text-decoration: none;
+  }
+`;
+export default Sidebar;
