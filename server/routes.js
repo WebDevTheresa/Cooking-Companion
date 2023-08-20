db66f6d1435cd5f9540a9ee902dc27efe18dd825
“Theresa <webdevtheresa@gmail.com>
2023-08-20 19:40:55 -0400

    2 new routes added

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,9 @@
-const { getIngredients, createUser } = require('./handlers');
+const {
+  getIngredients,
+  createUser,
+  deleteRecipe,
+  postARecipe,
+} = require('./handlers');
 
 //fill in squiggles with endpoint names
 
@@ -6,7 +11,8 @@ const router = require('express').Router();
 
 router.get('/getIngredients', getIngredients);
 router.post('/createUser', createUser);
-
+router.delete('/recipes/:id', deleteRecipe);
+router.post('/recipes', postARecipe);
 router.get('hello', (req, res) => {
   return res.status(200).json({ status: 200, message: 'hello from server' });
 });
