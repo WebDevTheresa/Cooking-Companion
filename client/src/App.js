e56e2efb9be3d0e5b804fefbc4a7cd9f74e1b7bb
“Theresa <webdevtheresa@gmail.com>
2023-08-23 21:22:14 -0400

    api added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import FoodSelection from './FoodSelection';
 import Home from './Home';
 import LoadRecipe from './LoadRecipe';
 import UserLikes from './UserLikes';
-// import PostRecipe from './PostRecipe';
 
-// import Navbar from './Navbar';
+// import PostRecipe from './PostRecipe';
+// import { getSearchRecipesbyIngredients } from './NutritionApi';
 
 const App = () => {
+  // const options = ('./nutritionApi');
+
+  // const apiOptions = {
+  //   ingredients: 'ingreidents',
+  //   number: '7',
+  //   ignorePantry: 'true',
+  //   ranking: '1',
+  // };
+  // console.log(apiOptions);
+
+  // const fetchRecipeApi = async () => {
+  //   try {
+  //     const response = await getSearchRecipesbyIngredients('ingredients');
+  //     console.log(response.data, 'data response');
+  //   } catch (error) {
+  //     console.error('An error occurred:', error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchRecipeApi();
+  // }, []);
+
   return (
     <Router>
       <Routes>
