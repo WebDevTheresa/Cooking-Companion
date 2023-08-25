e25238155b6cfcc9809ccca33c07191f4dbf17ab
“Theresa <webdevtheresa@gmail.com>
2023-08-24 22:10:59 -0400

    component created

---
+++ client/src/LikesBar.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import React from 'react';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+const LikesBar = () => {
+  return;
+};
+
+export default LikesBar;
