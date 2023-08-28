cf3a41239b3645188a5f51e15dc62c75f86d877b
“Theresa <webdevtheresa@gmail.com>
2023-08-27 20:26:49 -0400

    likes bar finished

--- client/src/LikesBar.js
+++ client/src/LikesBar.js
@@ -1,47 +1,40 @@
 import styled from 'styled-components';
-import { React, useEffect, useState } from 'react';
+import { React, useState } from 'react';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { useParams } from 'react-router-dom';
 
-const LikesBar = () => {
+const LikesBar = ({ recipe }) => {
   const [isLiked, setIsLiked] = useState(false);
   const [buttonActive, setButtonActive] = useState(false);
-  const [saveRecipe, setSaveRecipe] = useState();
   const [displayLikes, setDisplayLikes] = useState();
 
   console.log(isLiked);
 
   const handleButtonClick = () => {
     setIsLiked(!isLiked);
-  };
-  // debugger;
-  useEffect(() => {
-    // if (saveRecipe)
+
     fetch(`/recipeLikes`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ recipe: saveRecipe }),
+      body: JSON.stringify({ recipe }),
     })
       .then((response) => response.json())
       .then((data) => {
-        debugger;
-        setSaveRecipe();
+        // debugger;
+
         setDisplayLikes(data.recipe);
         setButtonActive(true);
         console.log('success message');
       })
       .catch((error) => console.log(error));
-  }, []);
-
-  const likesSaved = localStorage.getItem('recipe');
+  };
 
   return (
     <div>
       <RedButton onClick={handleButtonClick} active={isLiked}>
-        {isLiked ? <FavoriteBorderIcon /> : <FavoriteBorderIcon />}
+        <FavoriteBorderIcon />
       </RedButton>
     </div>
   );
