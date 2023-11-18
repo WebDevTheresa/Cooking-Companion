21544f03834eab5a409ece5ee633123c3a609e4e
webdevtheresa <webdevtheresa@gmail.com>
2023-11-18 13:40:44 -0500

    recipes

---
+++ server/data/Recipes.js
@@ -0,0 +1,345 @@
+const cookRecipe = [
+  {
+    _id: '0001',
+    recipeName: 'Broccoli and Cheddar Soup',
+    recipeDescription: 'Classic Hearty Soup',
+    recipeIngredients: [
+      {
+        ingredientName: 'Broccoli Florets',
+        amount: '2 cups',
+        ingredientName: 'Shredded Cheese',
+        amount: '1 Cup ',
+        ingredientName: 'Onion',
+        amount: '1',
+        ingredientName: 'Garlic',
+        amount: '2 Cloves',
+        ingredientName: 'chicken or Vegetable Broth',
+        amount: '4 Cups',
+        ingredientName: ' Milk',
+        amount: '1 Cup',
+        ingredientName: 'Salt and Pepper ',
+      },
+    ],
+    instructions: {
+      steps:
+        'In a large pot, sauté the onion and garlic until translucent. Add broccoli and broth, then simmer untilt he broccoli is tender. Use and imersion blender to puree the soup untl the soup is smooth. Stir in the milk and cheddar cheese until the cheese is melted. Season with salt and pepper to taste. Serve Hot!',
+    },
+  },
+
+  {
+    _id: '0002',
+    recipeName: 'Roasted Broccoli with Lemon and Parmesan',
+    recipeDescription: 'Tangy Cheesy Delight',
+    recipeIngredients: [
+      {
+        ingredientName: 'Broccoli Florets',
+        amount: '4 cups',
+        ingredientName: 'Olive Oil',
+        amount: '2 tablespoons ',
+        ingredientName: 'Lemon',
+        amount: '1 zested and juiced',
+        ingredientName: 'Parmesan Cheese',
+        amount: '1/2 Cup',
+        ingredientName: 'Salt and Pepper',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        'Preheat your oven to 400F (200C).Toss the broccoli florets in olive oil and spread them on a baking sheet. Roast for 20-25 minutes until the broccoli is crispy and slightly browned. Remove from the oven, sprinkle with lemon zest, lemon juice Parmesan cheese, salt, and pepper. Serve hot.',
+    },
+  },
+
+  {
+    _id: '0003',
+    recipeName: 'Chicken and Broccoli Stir-Fry',
+    recipeDescription: 'Changs Chicken',
+    recipeIngredients: [
+      {
+        ingredientName: 'Boneless Skinless chicken breasts',
+        amount: '2',
+        ingredientName: 'Broccoli Florets',
+        amount: '2 Cups',
+        ingredientName: 'Soy Sauce',
+        amount: '2 tablespoons',
+        ingredientName: 'Honey',
+        amount: '1 tablesoon',
+        ingredientName: 'garlic',
+        amount: '2 cloves',
+        ingredientName: 'Ginger',
+        amount: '1 teaspoon',
+        ingredientName: 'Vegetable Oil',
+        amount: '2 tablespoons',
+        ingredientName: 'Cooked Rice',
+        amount: '2 cups',
+      },
+    ],
+    instructions: {
+      steps:
+        'In a bowl, mix soy sauce, honey, garlic, and ginger. Set aside. Heat vegetable oil in a large pan or wok over high heat. Add chicken and stir-fry until cooked through. Add broccoli and continue stir-frying for a few more minutes. Pour the sauce over the chicken and broccoli and cook until the sauce thickens.Serve hot over cooked rice. ',
+    },
+  },
+
+  {
+    _id: '0004',
+    recipeName: 'Broccoli and Quinoa Salad',
+    recipeDescription: 'High Protein Low Calorie',
+    recipeIngredients: [
+      {
+        ingredientName: 'Quinoa',
+        amount: '2 Cups',
+        ingredientName: 'Broccoli Florets',
+        amount: '2 Cups',
+        ingredientName: 'Cranberries',
+        amount: '1/2 Cup dried',
+        ingredientName: 'Almonds',
+        amount: '1/4 cup chopped',
+        ingredientName: 'Feta',
+        amount: '1/4 cup',
+        ingredientName: 'Olive Oil',
+        amount: '2 tablespoons',
+        ingredientName: 'Balsamic Vinegar',
+        amount: '2 tablespoons',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        'In a large bowl, combine quinoa, broccoli, cranberries, almonds, and feta cheese. In a separate bowl, whisk together olive oil, balsamic vinegar, salt, and pepper. Drizzle the dressing over the salad and toss to combine. Serve chilled.',
+    },
+  },
+
+  {
+    _id: '0005',
+    recipeName: 'Broccoli and Mushroom Alfredo Pasta',
+    recipeDescription: 'Hearty Pasta Dish',
+    recipeIngredients: [
+      {
+        ingredientName: 'Fettuccine Pasta',
+        amount: '8 oz',
+        ingredientName: 'Broccoli Florets',
+        amount: '2 Cups',
+        ingredientName: 'sliced mushrooms',
+        amount: '1 Cup ',
+        ingredientName: 'Heavy Cream',
+        amount: '1 cup',
+        ingredientName: 'Parmesan cheese',
+        amount: '1/2 cup grated',
+        ingredientName: 'Garlic',
+        amount: '2 cloves',
+        ingredientName: 'Butter',
+        amount: '2 tablespoons',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        'Cook the pasta according to package instructions. Add broccoli during the last 3 minutes of cooking. Drain and set aside. In a skillet, melt the butter and sauté the garlic and mushrooms until tender. Stir in the heavy cream and Parmesan cheese, simmering until the sauce thickens. Season with salt and pepper, then toss the pasta and broccoli in the sauce. Serve hot. ',
+    },
+  },
+  {
+    _id: '0006',
+    recipeName: 'Classic Mashed Potatoes',
+    recipeDescription: 'Classic Mashed Potatoes',
+    recipeIngredients: [
+      {
+        ingredientName: 'Potato',
+        amount: '4 Russet peeled and cut into chucks',
+        ingredientName: 'Milk',
+        amount: '1/2 Cup',
+        ingredientName: 'sliced mushrooms',
+        amount: '1 Cup ',
+        ingredientName: 'Butter',
+        amount: '4 tablespoons',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        'Boil the potato chunks in a large pot of salted water until tender. Drain the potatoes and mash them with a potato masher or fork. Heat the milk and butter in a saucepan until the butter melts.Pour the warm milk mixture over the mashed potatoes and stir until creamy. ',
+    },
+  },
+
+  {
+    _id: '0007',
+    recipeName: 'Potato Leek Soup',
+    recipeDescription: 'Vegetarian Potato Soup',
+    recipeIngredients: [
+      {
+        ingredientName: 'Potato',
+        amount: '4 Russet peeled and cut into chucks',
+        ingredientName: 'Heavy Cream',
+        amount: '1/2 Cup',
+        ingredientName: 'Leeks',
+        amount: '2 Cup, clean and sliced ',
+        ingredientName: 'Chicken or Vegetable Broth',
+        amount: '4 cups',
+        ingredientName: 'Butter',
+        amount: '2 tablespoons',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        'In a large pot, sauté the leeks in butter until softened. Add the diced potatoes and broth. Simmer until the potatoes are tender. Use an immersion blender to puree the soup until smooth. Stir in the heavy cream and heat through. Season with salt and pepper. Serve hot.   ',
+    },
+  },
+
+  {
+    _id: '0008',
+    recipeName: 'Potato And Vegetable Hash',
+    recipeDescription: 'Potato Based vegan dish',
+    recipeIngredients: [
+      {
+        ingredientName: 'Potato',
+        amount: '2 diced Russet',
+        ingredientName: 'Bell Pepper',
+        amount: '1 Cup',
+        ingredientName: 'Onion',
+        amount: '1 Cup',
+        ingredientName: 'Zucchini',
+        amount: '1 cups',
+        ingredientName: 'Olive Oil',
+        amount: '2 tablespoons',
+        ingredientName: 'Paprika',
+        amount: '1 teaspoon',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        ' Heat olive oil in a skillet over medium-high heat. Add potatoes and cook until they begin to brown. Stir in bell peppers, onions, and zucchini. Cook until all the vegetables are tender.  ',
+    },
+  },
+
+  {
+    _id: '0009',
+    recipeName: 'Potato And Chorizo Tacos',
+    recipeDescription: 'Mexican Inspired',
+    recipeIngredients: [
+      {
+        ingredientName: 'Potato',
+        amount: '4 diced Russet',
+        ingredientName: 'Chorizo Sausage',
+        amount: '1/2 pound',
+        ingredientName: 'Tortillas',
+        amount: '8, size small',
+        ingredientName: 'Cheddar Cheese',
+        amount: '1 cups',
+        ingredientName: 'Salsa',
+        amount: 'toppings',
+        ingredientName: 'Sour Cream',
+        amount: 'toppings',
+        ingredientName: 'Cilantro',
+        amount: 'toppings',
+      },
+    ],
+    instructions: {
+      steps:
+        ' In a skillet, cook the chorizo until browned. Remove and set aside. In the same skillet, add the diced potatoes and cook until crispy and cooked through. Warm the tortillas in the oven or on a griddle. Assemble the tacos with chorizo, potatoes, cheese, and your choice of toppings.  ',
+    },
+  },
+
+  {
+    _id: '0010',
+    recipeName: 'Potato And Spinach Frittata',
+    recipeDescription: ' Meat Free Potato Dish',
+    recipeIngredients: [
+      {
+        ingredientName: 'Potato',
+        amount: '4 sliced thinly',
+        ingredientName: 'Spinach Leaves',
+        amount: '2 Cup',
+        ingredientName: 'Eggs',
+        amount: '8',
+        ingredientName: 'Milk',
+        amount: '1/2 cup',
+        ingredientName: 'Shreded Cheese',
+        amount: '1 cup',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        ' Preheat your oven to 350°F (175°C). In an oven-safe skillet, sauté the potatoes until tender. Add the spinach and cook until wilted. In a bowl, whisk together eggs, milk, cheese, salt, and pepper. Pour the egg mixture over the potatoes and spinach in the skillet. Bake in the oven for about 15-20 minutes or until the frittata is set and slightly browned on top. Slice and serve.          ',
+    },
+  },
+  {
+    _id: '0011',
+    recipeName: 'Carrot Ginger Soup',
+    recipeDescription: ' Meat Free carrot Dish',
+    recipeIngredients: [
+      {
+        ingredientName: 'Carrot',
+        amount: '1 Pound, peeled and sliced',
+        ingredientName: 'Onion',
+        amount: '1 Chopped',
+        ingredientName: 'Ginger',
+        amount: '2 tablespoons, minced',
+        ingredientName: 'Vegetable or Chicken Broth',
+        amount: '4 cups',
+        ingredientName: 'Olive Oil',
+        amount: '2 tablespoons',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        'In a large pot, sauté the onion and ginger in olive oil until the onion becomes translucent. Add the sliced carrots and broth to the pot. Simmer until the carrots are tender. Use an immersion blender to puree the soup until smooth. Season with salt and pepper, and garnish with fresh cilantro if desired. Serve hot.',
+    },
+  },
+
+  {
+    _id: '0012',
+    recipeName: 'Honey Glazed Carrots',
+    recipeDescription: ' Meat Free carrot Dish',
+    recipeIngredients: [
+      {
+        ingredientName: 'Carrot',
+        amount: '1 Pound, peeled and sliced',
+        ingredientName: 'Butter',
+        amount: '2 tablespoons',
+        ingredientName: 'Honey',
+        amount: '2 tablespoons',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        'In a skillet, melt the butter over medium heat. Add the sliced carrots and sauté until they start to soften. Drizzle honey over the carrots and continue to cook until they caramelize and become tender. Season with salt and pepper, and garnish with fresh parsley if desired. Serve as a side dish.  ',
+    },
+  },
+  {
+    _id: '0013',
+    recipeName: 'Carrot Raisin Salad',
+    recipeDescription: ' SWeet and Savory Carrot Dish',
+    recipeIngredients: [
+      {
+        ingredientName: 'Carrot',
+        amount: '4 cups',
+        ingredientName: 'Raisin',
+        amount: '1/2 cups',
+        ingredientName: 'Mayonanaise',
+        amount: '1/2 cup',
+        ingredientName: 'Honey',
+        amount: '2 teaspoons',
+        ingredientName: 'Lemon',
+        amount: '2 teaspoons',
+        ingredientName: 'Salt and Pepepr',
+        amount: 'to taste',
+      },
+    ],
+    instructions: {
+      steps:
+        ' In a large bowl, combine the grated carrots and raisins. In a separate bowl, whisk together mayonnaise, honey, lemon juice, salt, and pepper. Pour the dressing over the carrots and raisins, tossing to coat. Garnish with chopped nuts if desired. Serve chilled ',
+    },
+  },
+];
